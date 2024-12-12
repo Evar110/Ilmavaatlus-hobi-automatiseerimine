cece91941fa4d606f52bf06274988885d207642f
Evar110 <evar.110@hotmail.com>
2024-12-13 00:09:38 +0200

    Tegin funktsiooni, mis salvestab andmed exceli faili

--- katse(chrome).py
+++ katse(chrome).py
@@ -6,6 +6,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from bs4 import BeautifulSoup
 from datetime import datetime
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Alignment, Border, Side, Font
+import os
+
 
 # funktsioon andmete kogumiseks
 def andmete_kogumine(kellaaeg, kogutav, koht):
@@ -19,7 +23,7 @@ def andmete_kogumine(kellaaeg, kogutav, koht):
         driver.get(url)
 
         # Kuupäeva sisestamine
-        date_input = WebDriverWait(driver, 20).until(
+        date_input = WebDriverWait(driver, 20).until( # ootab 20s või kuni saab lisada kuupäeva
             EC.presence_of_element_located((By.ID, "ood-datepicker"))
         )
         date_input.clear()
@@ -27,14 +31,14 @@ def andmete_kogumine(kellaaeg, kogutav, koht):
         date_input.send_keys(Keys.RETURN)
         
         # Kellaaja sisestamine
-        time_input = WebDriverWait(driver, 20).until(
+        time_input = WebDriverWait(driver, 20).until( # ootab 20s või kuni saab lisada kellaaja
             EC.presence_of_element_located((By.ID, "ood-timepicker"))
         )
         time_input.clear()
         time_input.send_keys(kellaaeg)
         time_input.send_keys(Keys.RETURN)
 
-        # Ootab, kuni leht lõpetab andmete laadimise
+        # Ootab 20s või kuni leht lõpetab andmete laadimise
         WebDriverWait(driver, 20).until(
             lambda d: driver.execute_script("return jQuery.active == 0")
         )
@@ -45,7 +49,7 @@ def andmete_kogumine(kellaaeg, kogutav, koht):
         tabel = soup.find("table", class_="table")
 
         if tabel:
-            read = tabel.find_all("tr")[1:]  # Jätab vahele päise rida
+            read = tabel.find_all("tr")[1:]  # Jätab vahele päise rea
             for rida in read:
                 andmete_rida = rida.find_all("td")
                 if andmete_rida:
@@ -70,25 +74,139 @@ def andmete_kogumine(kellaaeg, kogutav, koht):
     except Exception as e:
         print(f"Tekkis viga: {e}")
 
-# funktsioon nädalapäeva leidmiseks
-def nädalapäev(kuupäev): #formaadis pp.kk.aaaa
+# funktsioon nädalapäeva saamiseks
+def nädalapäev(kuupäev): 
     nädalapäevad = ['E', 'T', 'K', 'N', 'R', 'L', 'P']
     
     kuupäeva_obj = datetime.strptime(kuupäev, "%d.%m.%Y")
     nädalapäeva_indeks = kuupäeva_obj.weekday()
     return f"{kuupäeva_obj.day}.{nädalapäevad[nädalapäeva_indeks]}."
 
-# funktsioon mis teisendab hektopaskalid mmHg-ks
+# funktsioon, mis teisendab hektopaskalid mmHg-ks
 def hPa_mmHg(paskalid):
     mmHg = str(round(float(paskalid) * 0.750062)) + "mmHg"
     return mmHg
     
 
-# funktsioon mis muudab tuule suuna kraad ilmakaareks
+# funktsioon, mis muudab tuule suuna kraad ilmakaareks
 def tuule_suund(kraadid):
     ilmakaared = ["Põhja", "Kirde", "Ida", "Kagu", "Lõuna", "Edela", "Lääne", "Loode", "Põhja"]
     suuna_indeks = int((int(kraadid) + 22.5) % 360 // 45)
     return ilmakaared[suuna_indeks]
+
+# Funktsioon, mis kontrollib kas on uus aasta
+def uus_aasta(kuupäev):
+    if int(kuupäev[:2]) == 1 and int(kuupäev[3:5]) == 1:
+        return True
+    else:
+        return False
+    
+# funktsioon, mis kontrollib kas on uus kuu
+def uus_kuu(kuupäev):
+    if int(kuupäev[:2]) == 1:
+        return True
+    else:
+        return False
+
+# funktsioon, mis tagastab kuu ja aasta, mida kasutaja sisestas    
+def kuu_ja_aasta(kuupäev):
+    järjend = []
+    kuud = ['Jaanuar', 'Veebruar', 'Märts', 'Aprill', 'Mai', 'Juuni', 'Juuli', 'August', 'September', 'Oktoober', 'November', 'Detsember']
+    kuu = kuud[int(kuupäev[3:5]) - 1]
+    järjend.append(kuu)
+    aasta = int(kuupäev[6:])
+    järjend.append(aasta)
+    return järjend
+    
+# funktsioon, mis kirjutab andmed Exceli faili
+def lisa_excelisse(failinimi, andmed, kuupäev):
+    """
+    Lisab andmed Exceli faili uuele reale, kui faili ei ole, loob selle.
+    Lisaks määrab joondamise, stiili ja muud visuaalsed elemendid.
+    """
+    try:
+        # Kuu ja aasta
+        kuu_ja_aasta_järjend = kuu_ja_aasta(kuupäev)
+        aasta = kuu_ja_aasta_järjend[1]
+        kuu = kuu_ja_aasta_järjend[0]
+
+        # Stiilid
+        keskele_joondus = Alignment(horizontal="center")
+        alläär = Border(bottom=Side(style="thin"))
+        suur_font = Font(size=18)
+
+        # Spetsiaalsed read
+        päis_rida = ["Kuupäev", "Hommiku t°", "Õhtu t°", "Õhurõhk", "Tuule suund"]
+        kuu_rida = ["", "", kuu]
+        aasta_rida = ["", "", aasta]
+
+        #Juhul, kui ei ole sellist faili, loob selle
+        if not os.path.exists(failinimi):
+            fail = Workbook()
+            excel = fail.active
+
+            # Lisab aasta
+            excel.append(aasta_rida)
+            for lahter in excel[excel.max_row]:
+                lahter.alignment = keskele_joondus
+                lahter.font = suur_font
+
+            # Lisab kuu
+            excel.append(kuu_rida)
+            for lahter in excel[excel.max_row]:
+                lahter.alignment = keskele_joondus
+
+            # Lisab päise
+            excel.append(päis_rida)
+            for lahter in excel[excel.max_row]:
+                lahter.alignment = keskele_joondus
+                lahter.border = alläär
+
+            # Lisab andmed
+            excel.append(andmed)
+            for lahter in excel[excel.max_row]:
+                lahter.alignment = keskele_joondus
+
+            # Salvestab andmed
+            fail.save(failinimi)
+
+        else:
+            fail = load_workbook(failinimi)
+            excel = fail.active
+            # Kui on uus aasta, lisab uue aasta, kuu ja päise
+            if uus_aasta(kuupäev):
+                excel.append(aasta_rida)
+                for lahter in excel[excel.max_row]:
+                    lahter.alignment = keskele_joondus
+                    lahter.font = suur_font
+
+                excel.append(kuu_rida)
+                for lahter in excel[excel.max_row]:
+                    lahter.alignment = keskele_joondus
+
+                excel.append(päis_rida)
+                for lahter in excel[excel.max_row]:
+                    lahter.alignment = keskele_joondus
+                    lahter.border = alläär
+
+            # Kui on uus kuu, lisab uue kuu
+            elif uus_kuu(kuupäev):
+                excel.append(kuu_rida)
+                for lahter in excel[excel.max_row]:
+                    lahter.alignment = keskele_joondus
+
+            # Lisab andmed        
+            excel.append(andmed)
+            for lahter in excel[excel.max_row]:
+                lahter.alignment = keskele_joondus
+        
+            # Salvestab andmed
+            fail.save(failinimi)
+
+    # Juhul, kui kasutaja jätab exceli faili lahti, kui programmi käivitab
+    except PermissionError:
+        print("Viga, ei saanud salvestada faili, kontrolli ega sul exceli fail avatud ei ole")
+
 
 andmed = []
 url = "https://www.ilmateenistus.ee/ilm/ilmavaatlused/vaatlusandmed/tunniandmed/"
@@ -100,7 +218,7 @@ chrome_driver_path = "C:/Program Files/chromedriver-win64/chromedriver.exe"  # A
 kuupäev = input("Sisesta kuupäev (formaadis pp.kk.aaaa): ")
 hommikune_aeg = input("Sisesta hommikune kellaaeg (ainult täistund, formaadis tt:00): ")
 ohtune_aeg = input("Sisesta õhtune kellaaeg (ainult täistund, formaadis tt:00): ")
-asukoht = input("Sisesta asukoht, kus andmeid kogub (asukoha nimi, suure algustähega): ") # Oleks vaja kuidaski kas manuaalselt luua järjendi, mis sisaldab kõik asukohad, või siis teha funktsioon, mis kogub kõik asukohad 
+asukoht = input("Sisesta asukoht, kus andmeid kogub (asukoha nimi, suure algustähega): ") # Oleks vaja kuidaski kas manuaalselt luua järjendi, mis sisaldab kõik asukohad, või siis teha funktsioon, mis kogub kõik asukohad ja laseb kasutajal valida asukohta
 print('\n')
 
 # Käivitab Chrome WebDriver
@@ -113,9 +231,10 @@ andmete_kogumine(hommikune_aeg, "hommik", asukoht)
 # Andmete kogumine õhtul (temperatuur, õhurõhk, tuule suund)
 andmete_kogumine(ohtune_aeg, "õhtu", asukoht)
 
-# Lõpeta WebDriver
+# sulgeb WebDriveri
 driver.quit()
 
+# Muudab andmed järjendi elemente, et vastaksid tahetud formaadile
 päev_nädalapäev = nädalapäev(kuupäev)
 andmed.insert(0, päev_nädalapäev)
 õhurõhk = andmed[3]
@@ -127,3 +246,8 @@ ilmakaar = tuule_suund(tuule_kraadid)
 andmed[4] = ilmakaar
 
 print("Kogutud andmed:", andmed)
+
+failinimi = "ilm.xlsx"
+lisa_excelisse(failinimi, andmed, kuupäev)
+
+print(f"Andmed salvestatud faili {failinimi}.")
